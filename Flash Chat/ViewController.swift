109c220859d4f1efb165db096b461d9d87d66ded
Estudiante 1 <45402024+victortejeda@users.noreply.github.com>
2022-09-04 21:18:26 -0400

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Victor Tejeda on 4/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
